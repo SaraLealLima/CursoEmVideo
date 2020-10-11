5bd964ee719840d2315d48132003efc6773a2825
SaraLealLima <saral.limasm@gmail.com>
2020-10-11 19:22:28 -0300

    Arquivos ignorados deletados

--- Aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 58
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel > 60) { //condição simples
-    console.log(`Tá rapido demais chefia, toma essa multa aqui pra você ficar suave`)
-}
-console.log(`Dirige com segurança aí parça`)
